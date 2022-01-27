b0bfa438fd2a3631f5b2be7a766a3ac9cb46b0fc
Zach Schumacher <zachary@simplebet.io>
2022-01-27 12:39:26 -0500

    dont buffer

--- tests/conftest.py
+++ tests/conftest.py
@@ -95,7 +95,7 @@ def pymssql_commands(server, database_name) -> PymssqlCommands:
 @pytest.fixture(scope="session", autouse=True)
 def mysql_setup(database_name, setup_sql_dir, server):
     conn = mysql.connector.connect(host=server, port=3307, user="pydapper", password="pydapper", autocommit=True)
-    cursor = conn.cursor(buffered=True)
+    cursor = conn.cursor()
     setup_sql = (setup_sql_dir / "mysql.sql").read_text()
     cursor.execute(setup_sql, multi=True)
     conn.close()

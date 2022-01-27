56b11033cd0a5b4de661f108acfbe47c5c0c947d
Zach Schumacher <zachary@simplebet.io>
2022-01-27 11:52:41 -0500

    remove commit

--- tests/databases/mysql.sql
+++ tests/databases/mysql.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS pydapper.owner (
     name text NOT NULL,
     PRIMARY KEY ( id )
 );
-COMMIT;
 
 CREATE TABLE IF NOT EXISTS pydapper.task (
     id          integer AUTO_INCREMENT,
@@ -11,17 +10,14 @@ CREATE TABLE IF NOT EXISTS pydapper.task (
     due_date    date NOT NULL,
     owner_id    integer NOT NULL,
     PRIMARY KEY ( id ),
-    FOREIGN KEY ( owner_id ) REFERENCES owner(id)
+    FOREIGN KEY ( owner_id ) REFERENCES pydapper.owner(id)
 );
-COMMIT;
 
 INSERT IGNORE INTO pydapper.owner (id, name) VALUES
 (1, 'Zach Schumacher') ;
-COMMIT;
 
 INSERT IGNORE INTO pydapper.task (id, description, due_date, owner_id) VALUES
 (1, 'Set up a test database', '2021-12-31', 1),
 (2, 'Seed the test database', '2021-12-31', 1),
 (3, 'Run the test suite', '2022-01-01', 1)
 ;
-COMMIT ;

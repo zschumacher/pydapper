02f9243ae087680ef15423b8ec99045802341161
Zach Schumacher <zachary@simplebet.io>
2022-08-07 22:32:46 -0400

    add doc example for one to many relationships

---
+++ docs_src/methods/query/one_to_many_query.py
@@ -0,0 +1,60 @@
+import typing
+
+import pydapper
+from dataclasses import dataclass
+
+
+@dataclass
+class Owner:
+    id: int
+    name: str
+    tasks: typing.List["Task"]
+
+
+@dataclass
+class Task:
+    id: int
+    description: str
+
+
+query = """
+select task.id as task_id,
+       owner.id as owner_id,
+       owner.name as owner_name,
+       task.description as description
+  from owner
+  join task on owner.id = task.owner_id 
+"""
+
+
+with pydapper.connect() as commands:
+    owners = dict()
+    for record in commands.query(query, buffered=False):
+        if (owner_id := record["owner_id"]) not in owners:
+            owners[owner_id] = Owner(id=owner_id, name=record["owner_name"], tasks=list())
+        owners[owner_id].tasks.append(Task(id=record["task_id"], description=record["description"]))
+
+
+print(list(owners.values()))
+"""
+[
+    Owner(
+        id=1,
+        name='Zach Schumacher',
+        tasks=[
+            Task(
+                id=1,
+                description='Set up a test database',
+            ),
+            Task(
+                id=2,
+                description='Seed the test database',
+            ),
+            Task(
+                id=3,
+                description='Run the test suite',
+            ),
+        ],
+    ),
+]
+"""

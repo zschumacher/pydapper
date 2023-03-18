7530c57c1cfebf1a5913fa34e1c02f0c4c79a9bb
Zach Schumacher <zschu15@gmail.com>
2023-03-18 17:29:55 -0500

    remove print

--- tests/test_bigquery/conftest.py
+++ tests/test_bigquery/conftest.py
@@ -23,8 +23,6 @@ AUTH = {
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pydapper%40pydapper.iam.gserviceaccount.com",
 }
 
-print(AUTH["private_key"])
-
 @pytest.fixture(autouse=True, scope="session")
 def write_auth_file():
     AUTH_DIR.mkdir(exist_ok=True)

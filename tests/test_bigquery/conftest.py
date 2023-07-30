e4da27593c87621c47478782daa4478589685ad8
Zach Schumacher <42360273+zschumacher@users.noreply.github.com>
2023-07-30 17:35:34 -0500

    🔧 use bigquery emulator for tests (#166)
    * pull request target for bigquery
    
    * emulate bigquery
    
    * pr
    
    * fmt
    
    * rm auth code
    
    * try inline options
    
    * pass project first
    
    * try to overide entrypoint
    
    * ovveride entrypoint
    
    * point to correct entrypint
    
    * try one line
    
    * try w/o a service
    
    * -d
    
    * don't install storage for GHA
    
    * rm extra line
    
    * fix test

--- tests/test_bigquery/conftest.py
+++ tests/test_bigquery/conftest.py
@@ -1,35 +1,10 @@
-import json
 import os
 import sys
 import uuid
-from pathlib import Path
 
 import pytest
 
 from pydapper.bigquery import GoogleBigqueryClientCommands
-
-AUTH_DIR = Path(__file__).parent / "auth"
-AUTH_FILE_PATH = AUTH_DIR / "key.json"
-
-AUTH = {
-    "type": "service_account",
-    "project_id": "pydapper",
-    "private_key_id": "08c8a357ab549f6d34f1705512bdb00c2efaf68f",
-    "private_key": os.getenv("GOOGLE_PRIVATE_KEY", "DUMMY").replace("\\n", "\n"),
-    "client_email": "pydapper@pydapper.iam.gserviceaccount.com",
-    "client_id": "105936813038399443987",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pydapper%40pydapper.iam.gserviceaccount.com",
-}
-
-
-@pytest.fixture(autouse=True, scope="session")
-def write_auth_file():
-    AUTH_DIR.mkdir(exist_ok=True)
-    with open(AUTH_FILE_PATH, "w") as auth_file:
-        json.dump(AUTH, auth_file)
 
 
 @pytest.fixture(scope="function")
@@ -46,32 +21,40 @@ def func_uuid():
 
 @pytest.fixture(scope="function")
 def task_table_name(python_version, func_uuid):
-    return f"pydapper.task_{python_version}_{func_uuid.hex}"
+    return f"pydapper.pydapper.task_{python_version}_{func_uuid.hex}"
 
 
 @pytest.fixture(scope="function")
 def owner_table_name(python_version, func_uuid):
-    return f"pydapper.owner_{python_version}_{func_uuid.hex}"
+    return f"pydapper.pydapper.owner_{python_version}_{func_uuid.hex}"
+
+
+@pytest.fixture
+def client(monkeypatch):
+    from google.api_core.client_options import ClientOptions
+    from google.auth.credentials import AnonymousCredentials
+    from google.cloud.bigquery import Client
+
+    options = ClientOptions(api_endpoint="http://localhost:9050")
+
+    client = Client(client_options=options, credentials=AnonymousCredentials(), project="pydapper")
+    yield client
 
 
 @pytest.fixture(scope="function")
-def creds_as_env_var(monkeypatch):
-    monkeypatch.setenv("GOOGLE_APPLICATION_CREDENTIALS", str(AUTH_FILE_PATH))
-
-
-@pytest.fixture(scope="function")
-def commands() -> GoogleBigqueryClientCommands:
+def commands(client) -> GoogleBigqueryClientCommands:
     from google.cloud.bigquery.dbapi import connect
 
-    with GoogleBigqueryClientCommands(connect()) as commands:
+    with GoogleBigqueryClientCommands(connect(client=client)) as commands:
         yield commands
 
 
 @pytest.fixture(scope="function")
-def bigquery_setup(setup_sql_dir, creds_as_env_var, python_version, owner_table_name, task_table_name):
+def bigquery_setup(client, setup_sql_dir, python_version, owner_table_name, task_table_name):
     from google.cloud.bigquery.dbapi import connect
 
-    conn = connect()
+    conn = connect(client=client)
+
     cursor = conn.cursor()
     owner = (setup_sql_dir / "bigquery" / "owner.sql").read_text().format(owner_table_name=owner_table_name)
     cursor.execute(owner)

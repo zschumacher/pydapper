ab6ff8f504c821d43247f8a19a9529d563ffc656
Zach Schumacher <zschu15@gmail.com>
2023-07-30 16:24:56 -0500

    rm auth code

--- tests/test_bigquery/conftest.py
+++ tests/test_bigquery/conftest.py
@@ -1,27 +1,10 @@
 import os
 import sys
 import uuid
-from pathlib import Path
 
 import pytest
 
 from pydapper.bigquery import GoogleBigqueryClientCommands
-
-AUTH_DIR = Path(__file__).parent / "auth"
-AUTH_FILE_PATH = AUTH_DIR / "key.json"
-
-AUTH = {
-    "type": "service_account",
-    "project_id": "pydapper",
-    "private_key_id": "08c8a357ab549f6d34f1705512bdb00c2efaf68f",
-    "private_key": os.getenv("GOOGLE_PRIVATE_KEY", "DUMMY").replace("\\n", "\n"),
-    "client_email": "pydapper@pydapper.iam.gserviceaccount.com",
-    "client_id": "105936813038399443987",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pydapper%40pydapper.iam.gserviceaccount.com",
-}
 
 
 @pytest.fixture(scope="function")

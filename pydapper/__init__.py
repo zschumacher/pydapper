9bbec6de44ecfa5eb33f5ab3d08b470670fd3d7a
Zach Schumacher <zschu15@gmail.com>
2025-01-21 19:14:06 -0600

    remove cx oracle import

--- pydapper/__init__.py
+++ pydapper/__init__.py
@@ -7,7 +7,6 @@ from .main import using
 from .main import using_async
 from .mssql import PymssqlCommands as _PymssqlCommands
 from .mysql import MySqlConnectorPythonCommands as _MySqlConnectorPythonCommands
-from .oracle import CxOracleCommands as _CxOracleCommands
 from .postgresql import AiopgCommands as _AioPgCommand
 from .postgresql import Psycopg2Commands as _Psycopg2Commands
 from .postgresql import Psycopg3Commands as _Psycopg3Commands
